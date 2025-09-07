f6b4961b4ce004056d1969c041b9195e92ee280a
InnaAlieva <alievainna123@gmail.com>
2025-09-07 22:37:00 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
@@ -7,6 +8,15 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "app.bundle.js",
+=======
+
+const path = require('node:path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+>>>>>>> 5e7c056 (Initial commit)
   },
   module: {
     rules: [
@@ -14,25 +24,48 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
+<<<<<<< HEAD
           loader: "babel-loader",
+=======
+          loader: 'babel-loader',
+>>>>>>> 5e7c056 (Initial commit)
         },
       },
       {
         test: /\.html$/,
         use: [
           {
+<<<<<<< HEAD
             loader: "html-loader",
+=======
+            loader: 'html-loader',
+>>>>>>> 5e7c056 (Initial commit)
           },
         ],
       },
       {
+<<<<<<< HEAD
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
+=======
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      }
+>>>>>>> 5e7c056 (Initial commit)
     ],
   },
   plugins: [
     new HtmlWebPackPlugin({
+<<<<<<< HEAD
       template: "./src/index.html",
       filename: "./index.html",
     }),
@@ -40,5 +73,10 @@ module.exports = {
       filename: "./style.css",
       chunkFilename: "[id].css",
     }),
+=======
+      template: './src/index.html',
+      filename: './index.html',
+    })
+>>>>>>> 5e7c056 (Initial commit)
   ],
 };
